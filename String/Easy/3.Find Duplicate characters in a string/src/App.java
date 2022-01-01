0ff0d8bb1cafa1df958a36c10fa34fc86ad48cab
sougata <sdas15226@gmail,com>
2022-01-01 09:56:49 +0530

    3.rd

--- String/Easy/3.Find Duplicate characters in a string/src/App.java
+++ String/Easy/3.Find Duplicate characters in a string/src/App.java
@@ -1,5 +1,26 @@
+import java.util.*;
 public class App {
+
+    static void DupliCharc(String str){
+
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<str.length();i++){
+            map.put(str.charAt(i),map.getOrDefault(str.charAt(i),0)+1);
+        }
+
+        /*Print duplicates in sorted order*/
+        for(Map.Entry<Character,Integer> mapElement : map.entrySet()){
+            char key = (char)mapElement.getKey();
+            int value =(int)mapElement.getValue();
+
+           // if (value > 1)
+            System.out.println(key
+                               + ", count = " + value);
+        }
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String str="aabbbcdeerfrgt";
+
+        DupliCharc(str);
     }
 }
